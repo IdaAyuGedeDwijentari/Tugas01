eec75f810ff6169220b72182470d98581564726f
IdaAyuGedeDwijentari <101305025+IdaAyuGedeDwijentari@users.noreply.github.com>
2022-04-07 09:58:23 +0800

    Tugas 1 OOP

---
+++ T1point01.java
@@ -0,0 +1,16 @@
+package t1point01;
+public class T1point01 {
+
+    public static void main(String[] args) {
+        String[]nama= {"Ida Ayu Gede Dwi Jentari","Ida Bagus Putu Yoga Wira Dangka","Gede Wahyu Mahendra","Anak Agung Bintang Purnama Desi","Ni Luh Putu Trisna Kantina Dewi"};
+        System.out.print("*************MENAMPILKAN 5 DATA NAMA MAHASISWA/I****************\n\n");
+        System.out.println("Mahasiswa 1 atas nama "+nama[0]);
+        System.out.println("Mahasiswa 2 atas nama "+nama[1]);
+        System.out.println("Mahasiswa 3 atas nama "+nama[2]);
+        System.out.println("Mahasiswa 4 atas nama "+nama[3]);
+        System.out.println("Mahasiswa 5 atas nama "+nama[4]);
+        System.out.println("\n****************************************************************");
+    }
+}
+    
+
---
+++ T1point02.java
@@ -0,0 +1,20 @@
+package t1point02;
+public class T1point02 {
+
+    public static void main(String[] args) {
+        String[][]nm=
+        {
+            {"Jeje","Reevin","Wahyu","Bintang","Kantina"},
+            {"TI-KAB","TI-DGM","TI-MTI","TI-KAB","TI-KAB"}
+        };
+        int[] x = {20103015,191011,201022,201011,201033};
+        
+        System.out.println("MENAMPILKAN DATA");
+        System.out.println(""+nm[0][0]+"\t\t"+x[0]+"\t"+nm[1][0]);
+        System.out.println(""+nm[0][1]+"\t\t"+x[0]+"\t"+nm[1][1]);
+        System.out.println(""+nm[0][2]+"\t\t"+x[0]+"\t"+nm[1][2]);
+        System.out.println(""+nm[0][3]+"\t\t"+x[0]+"\t"+nm[1][3]);
+        System.out.println(""+nm[0][4]+"\t\t"+x[0]+"\t"+nm[1][4]); 
+    }
+    
+}
